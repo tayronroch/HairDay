ea83f31501df839b5a32d1482f4b70d1507aa992
Tayron Rocha <tayronroch@gmail.com>
2026-06-18 12:03:57 -0300

    feat: add data-change module to handle date selection and trigger schedule updates

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@
 
 import dayjs from "./libs/dayjs.js";
 import { schedulesDay } from "./modules/page-load.js";
+import "./modules/form/data-change.js";
 
 import "./modules/form/submit.js";
 
@@ -10,4 +11,3 @@ import "./styles/form.css";
 import "./styles/schedule.css";
 
 schedulesDay();
-console.log(dayjs().format("DD/MM- HH:mmm"));
--- src/modules/form/hours-load.js
+++ src/modules/form/hours-load.js
@@ -5,6 +5,7 @@ import { hoursClick } from "./hours-click.js";
 const hours = document.getElementById("hours");
 
 export function hoursLoad({ date }) {
+  hours.innerHTML = "";
   const opening = openingHours.map((hour) => {
     const [scheduleHour] = hour.split(":");
 
