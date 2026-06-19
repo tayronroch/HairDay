a571f8908da2e186e246adf338050f3358dd6748
Tayron Roch <tayronroch@gmail.com>
2026-06-18 21:28:11 -0300

    feat: implement schedule display functionality and refactor schedule loading

--- src/modules/form/submit.js
+++ src/modules/form/submit.js
@@ -1,5 +1,6 @@
 import dayjs from "dayjs";
 import { scheduleNew } from "../../services/schedule-new.js";
+import { schedulesDay } from "../schedules/load.js";
 const form = document.querySelector("form");
 const clientName = document.getElementById("client");
 const selectedDate = document.getElementById("date");
@@ -25,10 +26,14 @@ form.onsubmit = async (event) => {
     }
 
     const [hour] = hourSelected.innerHTML.split(":");
-    const when = dayjs(selectedDate.value).add(hour, "hour").toISOString();
+    const when = dayjs(selectedDate.value)
+      .add(hour, "hour")
+      .format("YYYY-MM-DDTHH:mm:ss.SSS");
     const id = new Date().getTime();
 
     await scheduleNew(id, name, when);
+    await schedulesDay();
+    clientName.value = "";
   } catch (error) {
     alert("Não foi possível realizar o agendamento");
     console.log(error);
--- src/modules/schedules/load.js
+++ src/modules/schedules/load.js
@@ -1,11 +1,13 @@
 import { hoursLoad } from "../form/hours-load.js";
 import { schedulFetchByDay } from "../../services/schedule-fetch-by-day.js";
+import { schedulesShow } from "./show.js";
 
 const selectedDate = document.getElementById("date");
 
 export async function schedulesDay() {
   const date = selectedDate.value;
   const dailySchedules = await schedulFetchByDay({ date });
-  hoursLoad({ date });
-  console.log(dailySchedules);
+  schedulesShow({ dailySchedules })
+  hoursLoad({ date, dailySchedules });
+  
 }
